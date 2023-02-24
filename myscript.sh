4720e9971e97f485b4115293b04c2d67ade40c3a
노예찬 <71473074+noy3928@users.noreply.github.com>
2023-02-24 17:07:39 +0900

    feat : if, if-else

--- myscript.sh
+++ myscript.sh
@@ -6,9 +6,28 @@
 
 # VARIABLES
 # Uppercase by convention
-# NAME="Chan"
+NAME="hue"
 # echo "my name is $NAME"
 
 # USER INPUT
-read -p "Enter your name: " NAME
-echo "Hello $NAME, nice to meet you!"
+# read -p "Enter your name: " NAME
+# echo "Hello $NAME, nice to meet you!"
+
+# SIMPLE IF STATEMENT 
+if [ "$NAME" == "Chan" ]
+then
+    echo "Your name is Chan"
+else 
+    echo "Your name is NOT Chan"
+fi
+
+# IF-ELSE
+if [ "$NAME" == "Chan" ]
+then
+    echo "Your name is Chan"
+elif [ "$NAME" == "hue" ]
+then
+    echo "Your name is hue"
+else 
+    echo "Your name is NOT Chan or hue"
+fi
